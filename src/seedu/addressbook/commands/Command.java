3061650a2ea1d51578815e14668336cb3ae82722
e0003745 <e0003745@u.nus.edu>
2016-09-15 11:13:44 +0800

    Add abstract boolean isMutating()

--- src/seedu/addressbook/commands/Command.java
+++ src/seedu/addressbook/commands/Command.java
@@ -40,6 +40,11 @@ public abstract class Command {
      * Executes the command and returns the result.
      */
     public abstract CommandResult execute();
+    
+    /**
+     * Checks if the operation mutates data
+     */
+    public abstract boolean isMutating();
 
     /**
      * Supplies the data the command will operate on.
